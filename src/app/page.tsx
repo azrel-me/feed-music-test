4db9676196cbc7354213c707a32c7ae824567818
azrel-me <158019512+azrel-me@users.noreply.github.com>
2024-02-02 16:25:41 +0800

    fix: loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import ScrollPage from "@/components/ScrollPage";
 
-const loading = async () => {
-  return new Promise((resolve) => setTimeout(resolve, 2000));
+const sleep = async () => {
+  return await new Promise((resolve) => setTimeout(resolve, 2000));
 };
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
-  await loading();
+  await sleep();
 
   return (
     <>
